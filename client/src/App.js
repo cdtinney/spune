4a34074402ba4a15656f656fce4698f82ace0231
ctinney <ctinney@rossvideo.com>
2018-08-05 14:29:01 -0400

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {
+    response: '',
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({
+        response: res.express,
+      }))
+      .catch(console.error);
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw new Error(body.message);
+    }
+    
+    return body;
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <header className="app__header">
+          <h1 className="app__header__title">
+            Sune
+          </h1>
+        </header>
+        <p className="app__intro">
+          {this.state.response}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
+});
+
+if (process.env.NODE_ENV === 'production') {
+    // Serve static React files.
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    // Handle React routing, returning all requests to React app entry
+    // point.
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    }) ;
+}
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
