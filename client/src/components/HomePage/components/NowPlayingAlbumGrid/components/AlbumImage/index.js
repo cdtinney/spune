ea67a7033e0a1f4eba30431603a7f7c2a69a1aa7
ctinney <ctinney@rossvideo.com>
2018-10-24 20:35:43 -0400

    feat: add progressive album image loading

--- client/src/components/HomePage/components/NowPlayingAlbumGrid/components/AlbumImage/index.js
+++ client/src/components/HomePage/components/NowPlayingAlbumGrid/components/AlbumImage/index.js
@@ -4,10 +4,12 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import ProgressiveImage from 'react-progressive-image';
 
 export default function AlbumImage(props) {
   const {
     src,
+    placeholder,
     alt,
     className,
     width,
@@ -15,20 +17,35 @@ export default function AlbumImage(props) {
   } = props;
 
   return (
-    <img
+    <ProgressiveImage
       src={src}
+      placeholder={placeholder}
       alt={alt}
       className={className}
       style={{
         height: `${height}px`,
         width: `${width}px`,
       }}
-    />
+    >
+      {(src, loading) => (
+        <img
+          src={src}
+          alt={alt}
+          className={className}
+          style={{
+            height: `${height}px`,
+            width: `${width}px`,
+            opacity: loading ? 0.25 : 1,
+          }}
+        />
+      )}
+    </ProgressiveImage>
   );
 }
 
 AlbumImage.propTypes = {
   src: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
   className: PropTypes.string,
   height: PropTypes.number.isRequired,
   width: PropTypes.number.isRequired,
