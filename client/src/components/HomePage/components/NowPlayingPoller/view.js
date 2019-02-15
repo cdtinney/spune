2190474415c0a58b1ed07a11fdde2b9ef6453c94
cdtinney <colintinney@gmail.com>
2019-02-15 13:41:32 +0800

    test: add missing prop
    * add comments to prop types for clarity

--- client/src/components/HomePage/components/NowPlayingPoller/view.js
+++ client/src/components/HomePage/components/NowPlayingPoller/view.js
@@ -9,12 +9,21 @@ import PropTypes from 'prop-types';
  * Non-UI component.
  *
  * Polls the endpoint for updating the currently
- * played track every three seconds.
+ * played track on a specified interval.
  */
 export default class NowPlayingPoller extends Component {
   static propTypes = {
+    /**
+     * Whether or not the currently playing track is being loaded.
+     */
     loading: PropTypes.bool.isRequired,
+    /**
+     * Interval for updating, in milliseconds.
+     */
     interval: PropTypes.number.isRequired,
+    /**
+     * Updates the currently playing track.
+     */
     updateNowPlaying: PropTypes.func.isRequired,
   };
 
