99fcb1985447e962c7365574073d839af3f8ae5a
cdtinney <colintinney@gmail.com>
2019-02-15 13:41:32 +0800

    test: add missing prop
    * add comments to prop types for clarity

--- client/src/components/HomePage/components/NowPlayingPoller/__tests__/view.test.js
+++ client/src/components/HomePage/components/NowPlayingPoller/__tests__/view.test.js
@@ -6,6 +6,7 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   const props = {
     loading: false,
+    interval: 3000,
     updateNowPlaying: () => {},
   };
   ReactDOM.render(<NowPlayingPoller {...props} />, div);
