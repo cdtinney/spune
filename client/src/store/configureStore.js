5d3b4de3fed9764e69f2e74522a82037b61dd533
cdtinney <colintinney@gmail.com>
2019-02-15 13:27:42 +0800

    fix: update routing

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -1,39 +1,33 @@
-
-//////////////////////////
-// External dependencies//
-//////////////////////////
+///////////////////////////
+// External dependencies //
+///////////////////////////
 
 import { createStore, applyMiddleware } from 'redux';
-import { connectRouter, routerMiddleware } from 'connected-react-router'
+import { routerMiddleware } from 'connected-react-router'
 import { createHashHistory } from 'history'
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
-//////////////////////////
-// Internal dependencies//
-//////////////////////////
+///////////////////////////
+// Internal dependencies //
+///////////////////////////
 
-import reducer from '../reducers';
+import createRootReducer from '../reducers/createRootReducer';
+
+export const history = createHashHistory();
 
 export default function configureStore() {
-  const history = createHashHistory();
-  
-  // Create a new root reducer with router state.
-  const rootReducer = connectRouter(history)(reducer);
-
-  // Dispatch history actions.
-  const reduxRouterMiddleware = routerMiddleware(history);
-
-  const store = createStore(rootReducer, composeWithDevTools(
-    applyMiddleware(
-      thunk,
-      reduxRouterMiddleware,
+  const store = createStore(
+    createRootReducer(history),
+    undefined,
+    composeWithDevTools(
+      applyMiddleware(
+        thunk,
+        routerMiddleware(history), // For dispatching history actions
+      ),
     ),
-  ));
+  );
 
-  return {
-    store,
-    history,
-  };
+  return store;
 }
 
