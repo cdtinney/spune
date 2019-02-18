a8ea79d05a3631021a73eb0a45a9c27d19afca32
cdtinney <colintinney@gmail.com>
2019-02-18 19:45:54 +0800

    feat: make app bar transparent

--- client/src/components/TopAppBar/components/IconAvatar/index.js
+++ client/src/components/TopAppBar/components/IconAvatar/index.js
@@ -10,24 +10,32 @@ import Avatar from '@material-ui/core/Avatar';
 const styles = {
   avatar: {
     margin: 10,
+    boxShadow: '4px 4px 5px 0px rgba(0,0,0,0.29)',
   },
 };
 
 function IconAvatar(props) {
   const {
     alt,
+    title,
     src,
     classes,
   } = props;
 
   return (
     <div>
-      <Avatar alt={alt} src={src} className={classes.avatar} />
+      <Avatar
+        title={title}
+        alt={alt}
+        src={src}
+        className={classes.avatar}
+      />
     </div>
   );
 }
 
 IconAvatar.propTypes = {
+  title: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired,
   src: PropTypes.string.isRequired,
   classes: PropTypes.object.isRequired,
--- client/src/components/TopAppBar/index.js
+++ client/src/components/TopAppBar/index.js
@@ -5,9 +5,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 
 //////////////////////////
 // Internal dependencies//
@@ -16,34 +13,34 @@ import Typography from '@material-ui/core/Typography';
 import IconAvatar from './components/IconAvatar';
 
 const styles = {
-  flex: {
-    flexGrow: 1,
+  root: {
+    position: 'absolute',
+    display: 'flex',
+    width: '100%',
+  },
+  iconContainer: {
+    marginLeft: 'auto',
   },
 };
 
 function TopAppBar(props) {
   const {
-    title,
     userName,
     userImageUrl,
     classes,
   } = props;
   
   return (
-    <div>
-      <AppBar position="static" color="default">
-        <Toolbar>
-          <Typography variant="title" color="inherit" className={classes.flex}>
-            { title }
-          </Typography>
-          { userName &&
-            <IconAvatar
-              alt={userName}
-              src={userImageUrl}
-            />
-          }
-        </Toolbar>
-      </AppBar>
+    <div className={classes.root}>
+      { userName &&
+        <div className={classes.iconContainer}>
+          <IconAvatar
+            title={userName}
+            alt={userName}
+            src={userImageUrl}
+          />
+        </div>
+      }
     </div>
   );
 }
