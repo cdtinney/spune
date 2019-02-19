6e155440efb6c68b32ae192802d98a3f2cb5e105
cdtinney <colintinney@gmail.com>
2019-02-19 12:44:48 +0800

    fix: remove unused imports to fix build

--- client/src/components/HomePage/components/CoverOverlay/index.js
+++ client/src/components/HomePage/components/CoverOverlay/index.js
@@ -2,8 +2,7 @@
 // External dependencies //
 ///////////////////////////
 
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 
 //////////////////////////
