839490d67874d4b1e6e5eb9def3f0cf6a975f38a
cdtinney <colintinney@gmail.com>
2019-02-22 12:22:40 +0800

    use next typography version

--- client/src/theme/createTheme.js
+++ client/src/theme/createTheme.js
@@ -6,6 +6,12 @@ import { createMuiTheme } from '@material-ui/core/styles';
 
 export default function createTheme() {
   return createMuiTheme({
+    typography: {
+      // Use Typography v2 immediately.
+      // More info here:
+      // https://material-ui.com/style/typography/#migration-to-typography-v2
+      useNextVariants: true,
+    },
     palette: {
       type: 'dark',
     },
