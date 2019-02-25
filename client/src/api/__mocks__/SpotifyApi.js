c43d4d404b6e3a4c37a44e1f6189482a78031478
cdtinney <colintinney@gmail.com>
2019-02-25 19:02:39 +0800

    tests: add unit tests for spotify actions

--- client/src/api/__mocks__/SpotifyApi.js
+++ client/src/api/__mocks__/SpotifyApi.js
@@ -1,8 +1,12 @@
 export const mockGetMe = jest.fn();
+export const mockGetCurrentlyPlayingRelatedAlbums = jest.fn();
+export const mockGetMyCurrentPlaybackState = jest.fn();
 
 const mockClass = jest.fn().mockImplementation(() => {
   return {
     getMe: mockGetMe,
+    getCurrentlyPlayingRelatedAlbums: mockGetCurrentlyPlayingRelatedAlbums,
+    getMyCurrentPlaybackState: mockGetMyCurrentPlaybackState,
   };
 });
 
