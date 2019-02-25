f536ee6d0c1a3e1b21d5222e755190b35e1f83d2
cdtinney <colintinney@gmail.com>
2019-02-25 17:15:41 +0800

    tests: add App testing

--- client/src/__tests__/App.test.js
+++ client/src/__tests__/App.test.js
@@ -1,16 +1,30 @@
-import createRouterContext from 'react-router-test-context'
 import React from 'react';
+import createRouterContext from 'react-router-test-context'
 import { shallow } from 'enzyme';
-import { App } from '../App';
+import ConnectedApp, { App } from '../App';
 
-it('renders without crashing', () => {
-  const context = createRouterContext();
-  shallow(
-    <App
-      userAuthenticated={false}
-      fetchAuthUser={() => {}}
-    />, {
-      context,
-    },
-  );
+describe('App', () => {
+  describe('unconnected', () => {
+    it('renders without crashing', () => {
+      const context = createRouterContext();
+      shallow(
+        <App
+          userAuthenticated={false}
+          fetchAuthUser={() => {}}
+        />, {
+          context,
+        },
+      );
+    });
+  });
+
+  describe('connected', () => {
+    it('renders without crashing', () => {
+      shallow(
+        <ConnectedApp
+          history={{}}
+        />
+      );
+    });
+  });
 });
