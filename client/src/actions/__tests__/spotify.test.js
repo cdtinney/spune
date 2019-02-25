598240f6f7fe547914b768533d4f8df747b584db
cdtinney <colintinney@gmail.com>
2019-02-25 18:33:04 +0800

    tests: add unit tests for spotify actions

---
+++ client/src/actions/__tests__/spotify.test.js
@@ -0,0 +1,96 @@
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import * as spotifyActions from '../spotify';
+import SpotifyApi, {
+  mockGetMe,
+} from '../../api/SpotifyApi';
+
+jest.mock('../../api/SpotifyApi');
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+describe('spotifyActions', () => {
+  describe('async actions', () => {
+    describe('getMyInfo', () => {
+      beforeEach(() => {
+        SpotifyApi.mockClear();
+        mockGetMe.mockClear();
+      });
+
+      it('creates FETCH_USER_INFO_SUCCESS when fetching user info succeeds with a user with an image', () => {
+        mockGetMe.mockImplementation(() => Promise.resolve({  
+          id: 'fooId',
+          display_name: 'foo',
+          images: [{
+            url: 'fooImageUrl',
+          }],
+        }));
+    
+        const expectedActions = [{
+          type: spotifyActions.FETCH_USER_INFO_REQUEST,
+        }, {
+          type: spotifyActions.FETCH_USER_INFO_SUCCESS,
+          payload: {
+            info: {
+              id: 'fooId',
+              displayName: 'foo',
+              avatarImageUrl: 'fooImageUrl',
+            },
+          },
+        }];
+
+        const store = mockStore({});
+        return store.dispatch(spotifyActions.getMyInfo())
+          .then(() => {
+            expect(store.getActions()).toEqual(expectedActions);
+          });
+      });
+
+      it('creates FETCH_USER_INFO_SUCCESS when fetching user info succeeds with a user without an image', () => {
+        mockGetMe.mockImplementation(() => Promise.resolve({  
+          id: 'fooId',
+          display_name: 'foo',
+          images: undefined,
+        }));
+    
+        const expectedActions = [{
+          type: spotifyActions.FETCH_USER_INFO_REQUEST,
+        }, {
+          type: spotifyActions.FETCH_USER_INFO_SUCCESS,
+          payload: {
+            info: {
+              id: 'fooId',
+              displayName: 'foo',
+              avatarImageUrl: '',
+            },
+          },
+        }];
+
+        const store = mockStore({});
+        return store.dispatch(spotifyActions.getMyInfo())
+          .then(() => {
+            expect(store.getActions()).toEqual(expectedActions);
+          });
+      });
+
+      it('creates FETCH_USER_INFO_FAILURE when fetching user info fails', () => {
+        mockGetMe.mockImplementation(() => Promise.reject('foo'));
+    
+        const expectedActions = [{
+          type: spotifyActions.FETCH_USER_INFO_REQUEST,
+        }, {
+          type: spotifyActions.FETCH_USER_INFO_FAILURE,
+          payload: new Error('foo'),
+          error: true,
+        }];
+
+        const store = mockStore({});
+        return store.dispatch(spotifyActions.getMyInfo())
+          .then(() => {
+            expect(store.getActions()).toEqual(expectedActions);
+          });
+      });
+    });
+  });
+});
