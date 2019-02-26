dfb2e353c54177e87c3ab1f568ccc2723aee4182
cdtinney <colintinney@gmail.com>
2019-02-26 22:14:59 +0800

    tests: add unit testing for router init

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,55 +1,10 @@
 const express = require('express');
-const passport = require('passport');
-const paths = require('../config/paths');
-const spotify = require('./spotify');
+const initAuthRoutes = require('./auth');
+const initSpotifyRoutes = require('./spotify');
 
-const router = new express.Router();
-
-// Authorization
-
-router.get(
-  '/auth',
-  function(req, res) {
-    if (!req.user) {
-      res.json({});
-      return;
-    }
-
-    // TODO Need to find User
-    res.json({
-      user: req.user,
-    });
-  },
-);
-
-router.get(
-  '/auth/spotify',
-  passport.authenticate('spotify', {
-    scope: [
-      'user-read-private',
-      'user-read-email',
-      // Required for getting user's current playback info.
-      'user-read-playback-state',
-    ],
-  }),
-  function() {
-    // The request will be redirected to Spotify for authentication, so this
-    // function will not be called.
-  },
-);
-
-router.get(
-  '/auth/spotify/callback',
-  passport.authenticate('spotify', {
-    successRedirect: paths.clientHome,
-    failureRedirect: paths.clientLogin,
-  }),
-);
-
-// Spotify API
-
-router.get('/spotify/me', spotify.me);
-router.get('/spotify/me/player', spotify.mePlayer);
-router.get('/spotify/currently-playing/related-albums', spotify.currentlyPlayingRelatedAlbums);
-
-module.exports = router;
+module.exports = function initRouter() {
+  const router = new express.Router();
+  initAuthRoutes(router);
+  initSpotifyRoutes(router);
+  return router;
+};
