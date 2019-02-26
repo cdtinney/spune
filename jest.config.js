4bfadd87aee9989befa740faab54e23cf43518ca
cdtinney <colintinney@gmail.com>
2019-02-26 13:52:56 +0800

    tests: ignore coverage directory

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   collectCoverageFrom: [
     'server/**/*.{js}',
     '!server/server.js',
+    '!server/coverage/**',
   ],
   // By default, Jest runs in a JSDOM environment.
   // This has issues: https://mongoosejs.com/docs/jest.html
