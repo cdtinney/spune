1ac1990fce90fd466e289ca4aa646411b7c3a891
cdtinney <colintinney@gmail.com>
2019-02-26 18:59:57 +0800

    tests: add unit tests for auth modules

---
+++ server/spotify/auth/__tests__/refreshToken.test.js
@@ -0,0 +1,72 @@
+const refresh = require('passport-oauth2-refresh');
+const User = require('../../../database/schema/User');
+const refreshToken = require('../refreshToken');
+
+jest.mock('passport-oauth2-refresh', () => {
+  return {
+    requestNewAccessToken: jest.fn(),
+  };
+});
+
+jest.mock('../../../database/schema/User', () => {
+  return {
+    findOneAndUpdate: jest.fn(),
+  };
+});
+
+describe('refreshToken', () => {
+  it('returns a Promise', () => {
+    expect(refreshToken('foo')).toBeInstanceOf(Promise);
+  });
+
+  it('rejects the promise if the refresh strategy fails', () => {
+    refresh.requestNewAccessToken.mockImplementation((name, token, done) => {
+      done('fooError');
+    });
+
+    expect(refreshToken()).rejects.toEqual('fooError');
+  });
+
+  it('finds and updates the users access token when the refresh strategy succeeds', () => {
+    refresh.requestNewAccessToken.mockImplementation((name, token, done) => {
+      done(null, 'newAccessToken');
+    });
+
+    Date.now = jest.fn().mockImplementation(() => 123);
+    refreshToken('oldAccessToken');
+    expect(User.findOneAndUpdate.mock.calls[0][1]).toEqual({
+      $set: {
+        spotifyAccessToken: 'newAccessToken',
+        tokenUpdated: 123,
+      },
+    });
+  });
+
+  it('rejects the promise when the user update fails', () => {
+    refresh.requestNewAccessToken.mockImplementation((name, token, done) => {
+      done(null, 'newAccessToken');
+    });
+
+    User.findOneAndUpdate.mockImplementation((conditions, update, options, callback) => {
+      callback('fooError');
+    });
+
+    expect(refreshToken('accessToken')).rejects.toEqual('fooError');
+  });
+
+  it('resolves the promise when the user update fails', () => {
+    refresh.requestNewAccessToken.mockImplementation((name, token, done) => {
+      done(null, 'newAccessToken');
+    });
+
+    User.findOneAndUpdate.mockImplementation((conditions, update, options, callback) => {
+      callback(null, {
+        spotifyId: 'someUserId',
+      });
+    });
+
+    expect(refreshToken('accessToken')).resolves.toEqual({
+      spotifyId: 'someUserId',
+    });
+  });
+});
--- server/spotify/auth/passportStrategy.js
+++ server/spotify/auth/passportStrategy.js
@@ -3,6 +3,24 @@ const refresh = require('passport-oauth2-refresh');
 
 const User = require('../../database/schema/User');
 
+function verify(accessToken, refreshToken, expires_in, profile, done) {
+  User.findOrCreate({
+    // Used to find the document -- if it exists.
+    spotifyId: profile.id,
+  }, {
+    // These properties will be added/updated if it exists or not.
+    spotifyAccessToken: accessToken,
+    spotifyRefreshToken: refreshToken,
+    tokenUpdated: Date.now(),
+    expiresIn: expires_in,
+    displayName: profile.displayName,
+    photos: profile.photos,
+  }, function(err, user) {
+    console.log(`Created user ${user.spotifyId}.`);
+    return done(err, user);
+  });
+}
+
 module.exports = function passportStrategy() {
   const {
     SPOT_CLIENT_ID,
@@ -16,25 +34,11 @@ module.exports = function passportStrategy() {
       clientSecret: SPOT_CLIENT_SECRET,
       callbackURL: SPOT_REDIRECT_URI,
     },
-    function(accessToken, refreshToken, expires_in, profile, done) {
-      User.findOrCreate({
-        // Used to find the document -- if it exists.
-        spotifyId: profile.id,
-      }, {
-        // These properties will be added/updated if it exists or not.
-        spotifyAccessToken: accessToken,
-        spotifyRefreshToken: refreshToken,
-        tokenUpdated: Date.now(),
-        expiresIn: expires_in,
-        displayName: profile.displayName,
-        photos: profile.photos,
-      }, function(err, user) {
-        console.log(`Created user ${user.spotifyId}.`);
-        return done(err, user);
-      });
-    });
+    verify);
 
   // Adds a plugin to enable simple token swapping.
   refresh.use(spotifyStrategy);
   return spotifyStrategy;
 };
+
+module.exports.verify = verify;
