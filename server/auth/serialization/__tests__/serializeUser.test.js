d2590d76d649b3932d80b7569e50f46d0306be9e
cdtinney <colintinney@gmail.com>
2019-02-26 13:38:08 +0800

    tests: add unit testing for serialization

---
+++ server/auth/serialization/__tests__/serializeUser.test.js
@@ -0,0 +1,11 @@
+const serializeUser = require('../serializeUser');
+
+describe('serializeUser', () => {
+  it('serializes spotifyId and calls done() when given a valid user object', () => {
+    const mockDone = jest.fn();
+    serializeUser({
+      spotifyId: 'foo',
+    }, mockDone);
+    expect(mockDone).toHaveBeenCalledWith(null, 'foo'); // First arg is error
+  });
+});
