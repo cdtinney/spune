969b92d616c71bda34949952b5fcf1eeaadfa36e
cdtinney <colintinney@gmail.com>
2019-02-27 14:12:12 +0800

    refactor for clarity

--- server/spotify/api/helpers/getRelatedArtists.js
+++ server/spotify/api/helpers/getRelatedArtists.js
@@ -1,3 +1,17 @@
+const uniqueSet = require('../../../utils/uniqueSet');
+
+function validArtistIds(artistArrs) {
+  return artistArrs
+    // Flatten the 2D array.
+    .reduce((flattened, arr) => {
+      return flattened.concat(arr);
+    }, [])
+    // Filter out undefined artists.
+    .filter(artist => artist)
+    // Then, take only their IDs.
+    .map(artist => artist.id);
+}
+
 module.exports = async function getRelatedArtists(spotifyApi, trackArtists) {
   const relatedArtistsReqs = trackArtists
     .map(async (artistId) => {
@@ -10,17 +24,8 @@ module.exports = async function getRelatedArtists(spotifyApi, trackArtists) {
     });
 
   const relatedArtists = await Promise.all(relatedArtistsReqs);
-  const relatedArtistIds =
-    // First, flatten the arrays of artists.
-    relatedArtists.reduce((flattened, arr) => {
-      return flattened.concat(arr);
-    }, [])
-    // Filter out undefined artists.
-    .filter(artist => artist !== undefined && artist !== null)
-    // Then, take only their IDs.
-    .map(artist => artist.id);
-
-  // Make them unique.
-  const combinedArtistsIds = relatedArtistIds.concat(trackArtists);
-  return [...new Set(combinedArtistsIds)]; // Spread to convert to array
+  const relatedArtistIds = validArtistIds(relatedArtists);
+  return uniqueSet(relatedArtistIds);
 };
+
+module.exports.validArtistIds = validArtistIds;
