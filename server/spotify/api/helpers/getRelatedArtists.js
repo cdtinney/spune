3e84d602acf5eda4703010d4639b2bbe6aa17f42
cdtinney <colintinney@gmail.com>
2019-02-27 14:01:06 +0800

    refactor: use async/await for clarity

--- server/spotify/api/helpers/getRelatedArtists.js
+++ server/spotify/api/helpers/getRelatedArtists.js
@@ -1,24 +1,26 @@
 module.exports = async function getRelatedArtists(spotifyApi, trackArtists) {
   const relatedArtistsReqs = trackArtists
-    .map((artistId) => {
-      return spotifyApi.getArtistRelatedArtists(artistId)
-        .then(response => response.body.artists)
-        .catch(error => console.error(error));
+    .map(async (artistId) => {
+      try {
+        const response = await spotifyApi.getArtistRelatedArtists(artistId);
+        return response.body.artists;
+      } catch (error) {
+        console.error(error);
+      }
     });
-  return Promise.all(relatedArtistsReqs)
-    .then((relatedArtists) => {
-      // First, flatten the arrays of artists.
-      return relatedArtists.reduce((flattened, arr) => {
-          return flattened.concat(arr);
-        }, [])
-        // Filter out undefined artists.
-        .filter(artist => artist !== undefined && artist !== null)
-        // Then, take only their IDs.
-        .map(artist => artist.id)
-    // Finally, merge related artists and track artists into
-    // a set for uniqueness.
-    }).then((relatedArtistsIds) => {
-      const combinedArtistsIds = relatedArtistsIds.concat(trackArtists);
-      return [...new Set(combinedArtistsIds)]; // Spread to convert to array
-    });
+
+  const relatedArtists = await Promise.all(relatedArtistsReqs);
+  const relatedArtistIds =
+    // First, flatten the arrays of artists.
+    relatedArtists.reduce((flattened, arr) => {
+      return flattened.concat(arr);
+    }, [])
+    // Filter out undefined artists.
+    .filter(artist => artist !== undefined && artist !== null)
+    // Then, take only their IDs.
+    .map(artist => artist.id);
+
+  // Make them unique.
+  const combinedArtistsIds = relatedArtistIds.concat(trackArtists);
+  return [...new Set(combinedArtistsIds)]; // Spread to convert to array
 };
