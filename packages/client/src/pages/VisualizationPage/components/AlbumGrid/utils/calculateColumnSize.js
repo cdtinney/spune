0f68941459a4c12a3497b8fa4f73d6de2c174caf
cdtinney <colintinney@gmail.com>
2019-03-05 10:15:27 +0800

    tests: add unit testing for album grid utils

--- packages/client/src/pages/VisualizationPage/components/AlbumGrid/utils/calculateColumnSize.js
+++ packages/client/src/pages/VisualizationPage/components/AlbumGrid/utils/calculateColumnSize.js
@@ -3,6 +3,10 @@ export default function calculateColumnSize({
   minSize,
   maxSize,
 }) {
+  if (windowWidth === 0) {
+    return 0;
+  }
+
   for (let i = maxSize; i >= minSize; i--) {
     if (windowWidth % i === 0) {
       return i;
