fba4604496ab7905e0f46332fed4eb3c3926cdab
cdtinney <colintinney@gmail.com>
2019-03-11 17:54:15 +0800

    tests: add missing test

--- packages/client/src/utils/__tests__/shuffle.test.js
+++ packages/client/src/utils/__tests__/shuffle.test.js
@@ -1,5 +1,9 @@
 import shuffle from '../shuffle';
 
 describe('shuffle()', () => {
-
+  it('returns an array with the same elements', () => {
+    const arr = [ 'foo', 'bar', 'baz' ];
+    const result = shuffle(arr);
+    result.forEach(entry => expect(arr.indexOf(entry) !== -1));
+  });
 });
