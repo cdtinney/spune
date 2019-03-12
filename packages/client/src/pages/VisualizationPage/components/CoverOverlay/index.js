ec88aa8277f4233d1b3721cd4045d902caa801ce
cdtinney <colintinney@gmail.com>
2019-03-12 15:42:56 +0800

    animate color overlay

--- packages/client/src/pages/VisualizationPage/components/CoverOverlay/index.js
+++ packages/client/src/pages/VisualizationPage/components/CoverOverlay/index.js
@@ -3,11 +3,14 @@
 ///////////////////////////
 
 import React from 'react';
+import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 
-//////////////////////////
-// Internal dependencies//
-//////////////////////////
+///////////////////////////
+// Internal dependencies //
+///////////////////////////
+
+import './styles.css';
 
 const styles = {
   root: {
@@ -15,23 +18,54 @@ const styles = {
     width: '100%',
     height: '100%',
     zIndex: 1,
-    background:
-      'radial-gradient(' +
-        'ellipse at center,' +
-        'rgba(88, 0, 88, 0.65) 0%,' +
-        'rgba(4, 0, 33, 0.95) 100%,' +
-        'rgba(0,0,0,1) 50%)',
+    background: 'linear-gradient(-45deg, '
+      + '#EE7752, '
+      + '#E73C7E, '
+      + '#23A6D5, '
+      + '#23D5AB, '
+      + '#361fe2, '
+      + '#0eb71c, '
+      + '#0eb71c, '
+      + '#361fe2, '
+      + '#23D5AB, '
+      + '#23A6D5, '
+      + '#E73C7E, '
+      + '#EE7752) ',
+    opacity: 0.75,
+    backgroundSize: '1200% 1200%',
+    // 12 colours * 15 seconds to "focus" on each color =
+    // 180 seconds of animation.
+    animation: 'move-background 180s ease infinite alternate',
+  },
+  darken: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    background: 'radial-gradient(' +
+      'ellipse at center,' +
+        'rgba(0, 0, 0, 0.3) 0,' +
+        'rgba(0, 0, 0, 0.75) 75%,' +
+        'rgba(0, 0, 0, 1) 100%)',
   },
 };
 
-function ColorOverlay(props) {
-  const {
-    classes,
-  } = props;
-
+function ColorOverlay({
+  classes,
+}) {
   return (
-    <div className={classes.root}></div>
+    <div className={classes.root}>
+      <div className={classes.darken} />
+    </div>
   );
 }
+
+ColorOverlay.propTypes = {
+  classes: PropTypes.shape({
+    root: PropTypes.string.isRequired,
+    darken: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default withStyles(styles)(ColorOverlay);
