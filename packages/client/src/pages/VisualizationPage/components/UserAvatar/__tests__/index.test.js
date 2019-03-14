912e3ff99e5ab821e6a19283807cd5da2c89674f
cdtinney <colintinney@gmail.com>
2019-03-14 11:39:57 +0800

    tests: update prop names

--- packages/client/src/pages/VisualizationPage/components/UserAvatar/__tests__/index.test.js
+++ packages/client/src/pages/VisualizationPage/components/UserAvatar/__tests__/index.test.js
@@ -5,7 +5,7 @@ import UserAvatar from '../';
 describe('UserAvatar', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<UserAvatar title="" alt="" src="" />, div);
+    ReactDOM.render(<UserAvatar displayName="" thumbnailSrc="" />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
