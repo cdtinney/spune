f50d142415ac1a3d4ce9fa5b75fd0c19382b43ac
Colin Tinney <cdtinney@users.noreply.github.com>
2020-02-08 22:21:52 -0800

    refactor: add env var for server port

--- packages/client/src/setupProxy.js
+++ packages/client/src/setupProxy.js
@@ -1,10 +1,12 @@
 const proxy = require('http-proxy-middleware');
 
+const SERVER_PORT = 5001;
+
 module.exports = function setupProxy(app) {
   // In development, API requests must be forwarded
   // to the back-end development server.
   // In production, it's the same server.
   app.use(proxy('/api', {
-    target: 'http://localhost:5000/',
+    target: `http://localhost:${SERVER_PORT}/`,
   }));
 };
--- packages/server/src/initServer.js
+++ packages/server/src/initServer.js
@@ -1,6 +1,6 @@
 const logger = require('./logger');
 
-const DEFAULT_PORT = 5000;
+const DEFAULT_PORT = 5001;
 
 module.exports = function initServer(app) {
   const port = process.env.PORT || DEFAULT_PORT;
